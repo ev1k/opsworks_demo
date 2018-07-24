6c61b5ce29954c0b357eb081a6d3d0906edfeb84
Viktor Eremchenko <eremchenko@realtimeboard.com>
2018-07-24 15:27:37 +0500

    changed image

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <div align="center">
             <h1>Hello DevOps Team</h1>
             <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
-            <div align="center"><img src="https://s3.amazonaws.com/acloudguru-opsworkslab-donotdelete/logo-on-black-800.png"></div>
+            <div align="center"><img src="https://cdn-images-1.medium.com/max/512/1*a9CpAcHgQnK0p8_QVsidFQ.png"></div>
             <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
             <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>

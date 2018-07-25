82f6c02ec0d309caa77bcf4557410ab3ae406529
Viktor Eremchenko <eremchenko@realtimeboard.com>
2018-07-25 15:20:26 +0500

    changed title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     <body>
         <div align="center">
-            <h1>Hello DevOps Team</h1>
+            <h1>Hello DevOps Realtimeboard Team</h1>
             <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
             <div align="center"><img src="https://cdn-images-1.medium.com/max/512/1*a9CpAcHgQnK0p8_QVsidFQ.png"></div>
             <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
